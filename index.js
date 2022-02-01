748cd45638d88ad21928d0172f0b5f5035439226
RAVI SHANKAR PRATIHAST <ravipratihast@gmail.com>
2022-02-02 01:34:41 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const billAmount=document.querySelector("#bill-amount");
+// const billAmount=console.log("billAmount.value");
+
+// const checkButton = document.querySelector("#check-button");
+
+// checkButton.addEventListener("click",()=>console.log("clicked"));
+
+
+billAmount.addEventListener("input",()=>console.log(billAmount.value))
